062e08eb36a5926077ae892ba319414b96957d10
Charles Tinnell <Skip@Charless-MacBook-Pro.local>
2018-03-17 18:56:50 -0600

    more game logic, just adding in methods in the right spots, currently need to break out of loop where player is continually asked if they want to hit or stay

--- src/com/skilldistillery/cards/blackjack/Game.java
+++ src/com/skilldistillery/cards/blackjack/Game.java
@@ -38,7 +38,7 @@ public class Game {
 		if (player.playerBusts() == false) {
 			computer.getHand().addCard(deck.dealCard());
 			displayComputer();
-			// need to put in next deal method call here
+			playerTurn();
 		} else if (player.playerBusts() == true) {
 			ioManager.print(new LoseMessage());
 			playAgain();
@@ -46,16 +46,21 @@ public class Game {
 	}
 
 	public void playerTurn() {
+		//calls the hit or stay while the player 21 or under
 		while (player.playerBusts() == false) {
 			hitOrStay();
 		}
+		playAgain();
 	}
 
 	public void hitOrStay() {
+		//lets the player choose whether they want another card
 		ioManager.print(new TextWithNewLine(player.getName() + " do you want to stay or hit? (Please type h or s)"));
 		String hitOrStayValue = ioManager.getUserInput(new Text(""));
 		if (hitOrStayValue.equalsIgnoreCase("h")) {
 			player.getHand().addCard(deck.dealCard());
+			displayPlayer();
+			computerTurn();
 		} else if (hitOrStayValue.equalsIgnoreCase("s")) {
 			computerTurn();
 		} else {
@@ -64,20 +69,27 @@ public class Game {
 	}
 
 	public void computerTurn() {
+		//deals another card to the computer if they are under 17 and not busted
 		while (computer.playerBusts() == false) {
 			if (computer.getHand().getValueOfHand() < 17) {
 				computer.getHand().addCard(deck.dealCard());
+				displayComputer();
+				playerTurn();
 			} else {
 				playerTurn();
 			}
 		}
+		playAgain();
 	}
 	
 	public void playAgain() {
+		//to see if the player wants to play another game after 
 		ioManager.print(new TextWithNewLine("Do you want to play again " + player.getName() + "? (Please type y or n)"));
 		String playAgainValue = ioManager.getUserInput(new Text(""));
 		if (playAgainValue.equalsIgnoreCase("y")) {
-			//restart method goes here
+			computer.getHand().emptyHand();
+			player.getHand().emptyHand();
+			openingDeal();
 		} else if (playAgainValue.equalsIgnoreCase("n")) {
 			//exit method
 		} else {
@@ -86,6 +98,7 @@ public class Game {
 	}
 
 	public void compareHands() {
+		//to compare hands and get a winner for times when both players retire with a score of under 21
 		if (computer.getHand().getValueOfHand() > player.getHand().getValueOfHand()) {
 			ioManager.print(new LoseMessage());
 			playAgain();
@@ -96,16 +109,16 @@ public class Game {
 	}
 
 	public void displayPlayer() {
+		//displays user score and hand
 		ioManager.print(new TextWithNewLine("Your current hand is " + player.getHand().getPlayerHand()));
 		ioManager.print(new TextWithNewLine("That adds up to " + player.getHand().getValueOfHand()));
 
 	}
 
 	public void displayComputer() {
+		//displays computer hand and score
 		ioManager.print(new TextWithNewLine("The Dealer is showing " + computer.getHand().getPlayerHand()));
 		ioManager.print(new TextWithNewLine("That adds up to " + computer.getHand().getValueOfHand()));
 	}
 
 }
-
-// ioManager.print(new BlackJackMessage());
